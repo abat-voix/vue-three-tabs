ef698b9128dddc1f80976c6765f36cd4fb94e3ee
Igor Sokolov <is@letsnova.ru>
2022-07-01 21:34:18 +0300

    feat: init commit

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import 'vuetify/styles' // Global CSS has to be imported
+import { createApp } from 'vue'
+import { createVuetify } from 'vuetify'
+import App from './App.vue'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const app = createApp(App)
+const vuetify = createVuetify({components, directives})
+
+app.use(vuetify)
+
+app.mount('#app')
